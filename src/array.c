ad9832f185db18ed300cf7ccf8a117ecd0d092be
naoyafurudono <40193696+naoyafurudono@users.noreply.github.com>
2018-06-26 11:15:17 +0900

    Update array.c

--- src/array.c
+++ src/array.c
@@ -7,12 +7,20 @@ int A[100000];
 
 int main(){
   int i, lb, ub;
-  scanf("%d%d", &n, &k);
+    scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
-
+    lb = 0;
+    ub = n;
+    while(ub - lb > 1){
+        i = (lb + ub)/2;
+        if(A[i] >= k){
+            ub = i;
+        }
+        else{
+            lb = i;
+        }}
+    printf("%d\n",ub);
   return 0;
 }
